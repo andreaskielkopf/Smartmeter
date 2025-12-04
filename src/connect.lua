b546bde80e0355e70be4a841a1713eda08b64616
Andreas Kielkopf <andreas.kielkopf@gmail.com>
2025-12-04 20:33:05 +0100

    schlanker

--- src/connect.lua
+++ src/connect.lua
@@ -7,13 +7,14 @@ local function printStatus()
    if status == wifi.STA_GOTIP then
       local cfg=wifi.sta.getconfig(true)
       if cfg then
-         print ("\tCurrent station config")
+         print ("\tStation config")
          print ("\tssid    :" .. cfg.ssid)
          print ("\tpassword:" .. cfg.pwd)
          print ("\tbssid   :" .. cfg.bssid)
+         cfg=nil
       end
       print ("\tVerbunden mit " .. wifi.sta.getip())
-      return status
+      return true -- status
    else
       return false -- or nil ???
    end
@@ -22,12 +23,10 @@ end
 local eus=nil
 local eus_file='eus_params.lua'
 local function eusRead()
-   --   if not printStatus() then
    if file.exists(eus_file) then
       if not eus then
          print ('read file ' .. eus_file)
-         eus = dofile(eus_file)
-         -- Callbacks definieren
+         eus = dofile(eus_file) -- Callbacks definieren
          eus.connected_cb       = function() print "Wifi connected"    end
          eus.disconnected_cb    = function() print "Connection lost"   end
          eus.got_ip_cb          = function() print "IP erhalten "      end
@@ -41,8 +40,7 @@ end
 local function init()
    if not printStatus() then
       eus= eusRead()
-      if eus then
-         print 'init Connection'
+      if eus then         --         print 'init Connection'
          wifi.sta.config(eus)
          return printStatus() end
    end
@@ -77,66 +75,62 @@ end
 local function runOnCon(main)
    local retries=30
    tmr.create():alarm(500,tmr.ALARM_AUTO, function (t)
-      status= printStatus() -- dokumentieren
+      local status= printStatus() -- dokumentieren
       if not status then
          if retries > 0 then
-            print ("Warte auf Wlan " .. status .. " noch " .. retries)
+            print ("Warte "..retries.." auf Wlan")
             retries=retries-1
             return
-         end
-      end
+         end end
       t:stop()
       t:unregister()
+      retries=nil
       if status then
+         status=nil
          main() -- Weiter im Hauptprogramm
       else
          print ("Timeout: Verbindung fehlgeschlagen ")
          -- Fehlerbehandlung  in 5 Minuten erneut ?--
-         print ("Programm wird beendet")
-      end
-   end
-   )
-end
+         --         print ("Programm wird beendet")
+      end end ) end
 
-local function runOnConnection(main)
-   local retries=20
-   tmr.create():alarm(500,tmr.ALARM_AUTO,
-      function(ti)
-         status=wifi.sta.status()
-         if status == wifi.STA_GOTIP then
-            ti:stop()
-            ti:unregister()
-            local cfg=wifi.sta.getconfig(true)
-            if cfg then
-               print ("\tCurrent station config")
-               print ("\tssid    :" .. cfg.ssid)
-               print ("\tpassword:" .. cfg.pwd)
-               print ("\tbssid   :" .. cfg.bssid)
-            end
-            print ("\tVerbunden mit " .. wifi.sta.getip())
-            main() -- Weiter im Hauptprogramm
-         else
-            if retries <= 0 then
-               print ("Timeout: Verbindung fehlgeschlagen ")
-               ti:stop()
-               ti:unregister()
-               -- Fehlerbehandlung  in 5 Minuten erneut ?--
-               print ("Programm wird beendet")
-            else
-               print ("Warte auf Wlan " .. status .. " noch " .. retries)
-               retries=retries-1
-            end
-         end
-      end
-   )
-end
+--local function runOnConnection(main)
+--   local retries=20
+--   tmr.create():alarm(500,tmr.ALARM_AUTO,
+--      function(ti)
+--         status=wifi.sta.status()
+--         if status == wifi.STA_GOTIP then
+--            ti:stop()
+--            ti:unregister()
+--            local cfg=wifi.sta.getconfig(true)
+--            if cfg then
+--               print ("\tCurrent station config")
+--               print ("\tssid    :" .. cfg.ssid)
+--               print ("\tpassword:" .. cfg.pwd)
+--               print ("\tbssid   :" .. cfg.bssid)
+--            end
+--            print ("\tVerbunden mit " .. wifi.sta.getip())
+--            main() -- Weiter im Hauptprogramm
+--         else
+--            if retries <= 0 then
+--               print ("Timeout: Verbindung fehlgeschlagen ")
+--               ti:stop()
+--               ti:unregister()
+--               -- Fehlerbehandlung  in 5 Minuten erneut ?--
+--               print ("Programm wird beendet")
+--            else
+--               print ("Warte auf Wlan " .. status .. " noch " .. retries)
+--               retries=retries-1
+--            end
+--         end
+--      end
+--   )
+--end
 
-M.eus=eus
-M.printStatus=printStatus
-M.eusRead=eusRead
+--M.eus=eus
+--M.printStatus=printStatus
+--M.eusRead=eusRead
 M.init=init
 M.run=runOnCon
-
 -- usage: M.init() M.run( function() print "Hallo Albershausen" end )
 return M
-
