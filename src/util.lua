169f8a3e64735827c5b694ef7c762116003e83ff
Andreas Kielkopf <andreas.kielkopf@gmail.com>
2025-12-05 19:56:40 +0100

    kapselung, +methode fName()

--- src/util.lua
+++ src/util.lua
@@ -1,23 +1,33 @@
 -- Einige eigene funktionen
-local M={}
-local function print3d(v0)
---   if not v0 then print 'nil' return end -- nullpointer
-   if type(v0) ~= "table" then print(v0) return end
-   if #v0 == 0 then print 'empty{}' return end
-   for k1,v1 in pairs(v0) do
-      if type(v1) == "table" then
-         print(" "..k1.." \t={" )
-         for k2,v2 in pairs(v1) do
-            if type(v2) == "table" then
-               print("\t "..k2.." \t={" )
-               for k3,v3 in pairs(v2) do
-                  print("\t\t "..k3.." \t= "..v3 ) end
-               print("\t ".." \t} " )
-            else
-               print("\t "..k2.." \t= "..v2 ) end end
-         print("\t ".." \t} " )
-      else
-         print(" "..k1.." \t= "..v1 ) end end end
+do
+   local M={}
+   local function print3d(v0)
+      --   if not v0 then print 'nil' return end -- nullpointer
+      if type(v0) ~= "table" then print(v0) return end
+      if #v0 == 0 then print 'empty{}' return end
+      for k1,v1 in pairs(v0) do
+         if type(v1) == "table" then
+            print(" "..k1.." \t={" )
+            for k2,v2 in pairs(v1) do
+               if type(v2) == "table" then
+                  print("\t "..k2.." \t={" )
+                  for k3,v3 in pairs(v2) do
+                     print("\t\t "..k3.." \t= "..v3 ) end
+                  print("\t ".." \t} " )
+               else
+                  print("\t "..k2.." \t= "..v2 ) end end
+            print("\t ".." \t} " )
+         else
+            print(" "..k1.." \t= "..v1 ) end end end
 
-M.print3d=print3d
-return M
+   local function fName(name) -- usage: filename=fName(a or b or c)
+      local lc=table.concat({name,'.lc'})
+      if file.exists(lc) then return lc end
+      local lua=table.concat({name,'.lua'})
+      if file.exists(lua) then return lua end
+      return nil end
+
+   M.fName=fName
+   M.print3d=print3d
+   return M
+end
