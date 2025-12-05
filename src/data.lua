5fa3f6d7e364c18af3128716e029188dadf6cc14
Andreas Kielkopf <andreas.kielkopf@gmail.com>
2025-12-05 19:57:06 +0100

    erweitert

--- src/data.lua
+++ src/data.lua
@@ -1,118 +1,115 @@
 do
    local M={}
-   local _file='2025-12-01'
-   --local heap=node.heap
+   local util=require 'util'
 
-   --fake={  heap=node.heap,
-   --   ['2025-12-01']=test,
-   --   ['2025-12-02']={
-   --      {1,2,3,4,5,6,7,8,9,},
-   --      {9,8,7,6,5,4,3,2,1},
-   --   },
-   --}
-
-   local function getD(filename) -- aufruf mit dem gewünschten datum
-      if not filename then filename=_file end
-      local lc=filename..'.lc'
-      if file.exists(lc) then
-         filename=lc lc=nil
-      else
-         filename=filename..'.lua'
-      end
-      print (filename)
-      if not file.exists(filename) then return {} end
-      local stunden={}
-      function date(d)
-         if #d == 1 then stunden.datum=d[1] end
-         d=nil
-      end
-      function hour(h)
-         if #h == 2 then
-            local z={}
-            for k,v in pairs(h[2]) do
-               if v>0 then z[k]=v end
-               --            v=nil
-            end
-            h[2]=nil
-            stunden[h[1]]=z
-            z=nil
-            print(h[1])
-            h[1]=nil
-         end
-         h=nil
-      end
-      dofile(filename)
-      filename=nil
-      print "done"
-      return stunden
+   -- liefert den Datensatz für die angegebene Stunde aus den vorhandenen Dateien
+   -- oder einen leeren Datensatz für diese Stunde als Liste (einfach durchnummeriert)
+   -- Tabelle {Stunde, {0 bis zu 60 x(Takte je Minute)}}
+   -- Beispiel: {23,{}} oder {7,{0,99}} oder {0,{99,25,72,5,0,0,12,0,0}}
+   local function getHour(tag,stunde) -- aufruf mit dem gewünschten datum
+      local filename= util.fName(tag or today or '2025-12-01')
+      stunde=stunde or 15 -- default 15:00 Uhr bis 15:59:59
+      local erg
+      print ("read",filename or tag,stunde)
+      if filename then
+         function date(d) end -- alternative ohne print
+         -- function date(d) if type(d)=='table' and #d==1 then
+         --    print("date",d[1]) end end
+         function hour(h)
+            if not erg and type(h)=='table' and #h==2 and
+               type(h[1])~='table' and h[1]==stunde then
+               --               print('a=',h[1])
+               if type(h[2])=='table' then
+                  --                  print('b=',h[2])
+                  erg=h
+               end end end
+         dofile(filename) end
+      return erg or {stunde,{}}
    end
 
-   local function setD(data) -- consumer = verbraucht die daten
-      if not data then return end
-      if not data.datum then return end
+   -- liefert den kompletten Datensatz eines Tages aus den vorhanden Dateien,
+   -- oder einen leeren Datensatz für diesen Tag als verschachtelte Tabelle
+   -- Tabelle {Tag, {Stunde1, Stunde2, Stunde3 ...}
+   -- Beispiel{'2025-12-01',{}}
+   --
+   local function getDay(tag) -- aufruf mit dem gewünschten datum
+      local filename=util.fName(tag or today or '2025-12-01')
+      local stunden={}
+      print (filename or tag)
+      if filename then
+         function date(d)
+            if #d == 1 then stunden.datum=d[1] end
+            d=nil
+         end
+         function hour(h)
+            if #h == 2 then -- print(h[1])
+               local z={}
+               for k,v in pairs(h[2]) do
+                  if v>0 then z[k]=v end
+               end
+               stunden[h[1]]=z
+            end end
+         dofile(filename) -- print ("done",filename)
+      end
+      return stunden end
+
+   local function toLines(day) -- consumer = verbraucht die daten und liefert textzeilen
+      if not day or not day.datum then return end
       local lines={}
       local line
-      for k,v in pairs(data) do
-         collectgarbage()
-         print("heap("..k..")="..node.heap())
+      for k,v in pairs(day) do
+         -- print("heap(",k,node.heap())
          if k=='datum' then
-            line="date{'"..v.."'}"
+            line=table.concat({"date{'",v,"'}"})
          elseif type(v) =="table" then-- stundenweise
-            --            local rest="}}"
-            local buf={}
-            local q="}}"
---            print("insert("..q..")")
-            table.insert(buf,1,q)
+            local buf={"}}"}
             for n=60,1,-1 do -- 60 minuten
                local w=v[n]
                if w and w>0 then
-                  --                  rest=w..','..rest
---                  print("insert("..w..")")
                   table.insert(buf,1,w)
                elseif #buf>1 then
-                  --                  rest='0,'..rest
-                  local n="0"
---                  print("insert("..n..")")
-                  table.insert(buf,1,n)
-               end
-            end
---            u= require("util")
---            u.print3d(buf)
-            local rest= table.concat(buf,',')
-            line="hour{"..k..",{"..rest
+                  table.insert(buf,1,'0') -- nix=0
+               end end -- util.print3d(buf)
+            --            local rest= table.concat(buf,',')
+            line=table.concat({"hour{",k,",{",
+               table.concat(buf,',')})
          end
-         data[k]=nil
+         day[k]=nil
          if line then
-            --         table.insert(lines,line)
-            print (line)
+            table.insert(lines,line) -- print (line)
             line=nil
-         end
-      end
-      data=nil
-      --   table.sort(lines)
-      return lines
-         --   if not filename then filename='2025-12-01' end
-         --   print (filename..'.lua')
-         --   if not file.exists(filename..'.lua') then return {} end
-         --   local stunden={}
-         --   function date(d)
-         --      if #d == 1 then stunden.datum=d[1] end
-         --   end
-         --   function hour(h)
-         --      if #h == 2 then stunden[h[1]]=h[2] end
-         --   end
-         --   dofile(filename..'.lua')
-         --   util.print3d(stunden)
-         --   return stunden
+         end end
+      day=nil -- table.sort(lines)
+      return lines end
+
+   local function testA()
+      collectgarbage()
+      print('getDay()',node.heap())
+      local day= getDay()
+      collectgarbage()
+      print('toLines',node.heap())
+      local lines= toLines(day)
+      day=nil -- optioal ???
+      collectgarbage()
+      print('umgewandelt',node.heap())
+      lines=nil
+      collectgarbage()
+      print('sauber',node.heap())
    end
 
-   local erg= getD(_file)
-   print(node.heap())
-   print "got it"
-   erg= setD(erg)
-   print(node.heap())
-   erg=nil
+   local function testB()
+      for stunde= 0,23 do
+         print(getHour('2025-12-01',stunde))
+      end
+   end
+
+   testB()
    --   util.print3d(erg)
+   M.getHour=getHour    -- get Hour from Storage
+   M.getDay=getDay      -- get Day from Storage
+   M.dayToLines=toLines -- convert Day to lines{} for html
+   --   M.writeDay=writeDay
+   --   M.writeHour=writeHour
 
    --   return M
 end
