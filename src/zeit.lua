961ea482a96c2af4198d26c5b9beaa355b20008f
Andreas Kielkopf <andreas.kielkopf@gmail.com>
2025-12-05 10:02:11 +0100

    Tips angewendet, Heap verringert

--- src/zeit.lua
+++ src/zeit.lua
@@ -1,47 +1,46 @@
-local M={}
+-- Eine Interne Uhr auf dem laufenden halten
+jetzt={} -- globale zeittabelle anlegen
+do
+   local M={}
 
-local function dateTable(sec) -- unix-Zeit in Tabelle umwandeln
-   if not sec then sec, usec, rate=rtctime.get() end
-   local tb=rtctime.epoch2cal(sec)
-   tb.unix=sec
-   --   print("Sec=" .. sec)
-   return tb end
+   local function dateTable(sec) -- unix-Zeit in Tabelle umwandeln
+      sec=sec or rtctime.get()-- if not sec then sec, usec, rate=rtctime.get() end
+      local tb=rtctime.epoch2cal(sec)
+      tb.unix=sec -- tabelle ergänzen um unix-timestamp
+      return tb end  
 
-jetzt={} -- globale zeittabelle
---local function heuteIst()
---   return 'heute ist '..jetzt.heute..string.format("(%02d:%02d)",jetzt.stunde,jetzt.minute)
---end
-
-local function zeit(d) --Tabelle in Zeitwerte für jetzt umwandeln
-   if not d then d = dateTable() end
-   local heute = string.format("%d-%02d-%02d", d.year,d.mon,d.day)
-   local stunde = d.hour
-   local minute = d.min
-   if minute ~= jetzt.minute then      --      print (heuteIst())
-      print ('heute ist '..heute.."("..stunde..":"..minute..')')
+   local function zeit(d) -- Tabelle in Zeitwerte für jetzt umwandeln
+      d=d or dateTable() -- if not d then d = dateTable() end
+      local heute = string.format("%d-%02d-%02d", d.year,d.mon,d.day)
+      local stunde = d.hour
+      local minute = d.min
+      if minute ~= jetzt.minute then
+         --         local t={'heute ist ',heute,"(",stunde,":",minute,')'}
+         --         print(table.concat(t))
+         print(table.concat({'heute ist ',heute,"(",stunde,":",minute,')'}))
+      end
+      return { unix=d.unix, heute=heute, stunde=stunde, minute=minute }
    end
-   return { unix=d.unix, heute=heute, stunde=stunde, minute=minute }
-end
 
+   local was=0
+   local function timeTicker(ti)
+      local sec = rtctime.get() -- beachte nur die Änderung der Sekunden
+      if sec == was then return end
+      local t=dateTable(sec)
+      jetzt = zeit(t) -- global eintragen
+   end
+  
+   local function init() -- einmal pro Sekunde genauer brauchts nicht sein
+      sntp.sync(nil,nil,nil,1)
+      tmr.create():alarm(1000,tmr.ALARM_AUTO,timeTicker)
+   end
 
-local was=0
-local function timeTicker(ti)
-   local sec = rtctime.get() -- beachte nur die Änderung der Sekunden
-   if sec == was then return end
-   local t=dateTable(now)
-   --   if t.minute == jetzt.minute then return end
-   jetzt = zeit(t) -- global eintragen
-   --   util.print3d(jetzt)
+   -- Init sofort ausführen und nicht exportieren. Das schont den Heap
+   -- M.init=init
+   init()
+   M.get=zeit
+   M.dateTable=dateTable
+   M.jetzt=jetzt
+   --M.jetzt=jetzt
+   return M
 end
--- einmal pro Sekunde genauer brauchts nicht sein
-local function init()
-   sntp.sync(nil,nil,nil,1)
-   tmr.create():alarm(1000,tmr.ALARM_AUTO,timeTicker)
-end
---printT3d(d)
-
-M.init=init
-M.get=zeit
-M.dateTable=dateTable
---M.jetzt=jetzt
-return M
