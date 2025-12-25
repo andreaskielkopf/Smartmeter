96f7630851dfa825fd96fd6b689cfee5b21c5d6b
andreas <andreas@manjaro25>
2025-12-25 18:51:07 +0100

    -strip scheint bereits included

--- src/lfs.sh
+++ src/lfs.sh
@@ -5,7 +5,7 @@ luac.cross -o ./lc/smartmeter.img -f *.lua
 
 for F in *.lua; do luac.cross -s -o "./nodebug/${F%.*}.lc" $F ;done
 
-luac.cross -s -o ./nodebug/smartmeter.img -f *.lua
+# luac.cross -s -o ./nodebug/smartmeter.img -f *.lua
 
 echo "first upload 'smartmeter.img', '_init.lc', 'start.lc'"
 echo "then execute:"
