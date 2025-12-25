33a60b96b85a5be8417561d61c88614f6fa23815
andreas <andreas@manjaro25>
2025-12-25 16:13:10 +0100

    automatisches speichern von tag und stunde

--- src/smartmeter.lua
+++ src/smartmeter.lua
@@ -13,9 +13,8 @@ do
    print "load smartmeter"
    local hour_=require 'hour'
    local day_=require 'day'
-   local heute
-   local gestern
-   local stunde={}
+
+   local stunde
 
    local function test()
       print ''
@@ -23,11 +22,31 @@ do
       day_.test()
    end
 
-   local function init()
-      print ("smartmeter gestartet")
-      gestern={}
-      heute={}
---      test()
+   local function init(tag_,stunde_)
+      print ("smartmeter init", tag_,stunde_)
+      local tag = day_.create(tag_)
+      print (table.concat(day_.toLua(tag),'\n'))
+      stunde = hour_.get(tag_,stunde_)
+      print (hour_.toLua(stunde))
+      --      test()
+   end
+
+   local function nextMin(tag_neu,stunde_neu,minute_neu)
+      if jetzt and jetzt.heute then
+         if not stunde then init(tag_neu,stunde_neu) end
+         local tag_alt,stunde_alt,minute_alt=jetzt.heute,jetzt.stunde,jetzt.minute
+         print(table.concat({'jetzt ist ',tag_neu,"(",stunde_neu,":",minute_neu,')'}))
+         if stunde_alt~=stunde_neu then -- stunde speichern
+            hour_.append(tag_alt,stunde)
+            if tag_alt~=tag_neu then -- tag anpassen
+               day_.create(tag_neu)
+               day_.compile(tag_alt)
+            end
+            stunde= hour_.get(tag_neu,stunde_neu) -- neue stunde vorbereiten
+         end
+      else
+         print('Minute = ',minute_neu)
+      end
    end
 
    local function data(datum)
@@ -52,6 +71,8 @@ do
 
    M.data=data
    M.init=init
+   M.next=nextMin
+   M.stunde=stunde
    print "end smartmeter"
    return M
 end
