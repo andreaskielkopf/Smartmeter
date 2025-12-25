9d0ae6f9395340d11732c3843e16ae9606fa7bf7
andreas <andreas@manjaro25>
2025-12-25 16:35:38 +0100

    kompaktiert

--- src/smartmeter.lua
+++ src/smartmeter.lua
@@ -9,32 +9,31 @@
 -- DHT
 -- SPI UCG ST7735 ??
 do
-   local M={}
    print "load smartmeter"
+   local M={}
    local hour_=require 'hour'
    local day_=require 'day'
-
    local stunde
 
-   local function test()
-      print ''
-      hour_.test()
-      day_.test()
-   end
+   --   local function test()
+   --      print ''
+   --      hour_.test()
+   --      day_.test()
+   --   end
 
    local function init(tag_,stunde_)
       print ("smartmeter init", tag_,stunde_)
       local tag = day_.create(tag_)
-      print (table.concat(day_.toLua(tag),'\n'))
+      --      print (table.concat(day_.toLua(tag),'\n'))
       stunde = hour_.get(tag_,stunde_)
-      print (hour_.toLua(stunde))
+      --      print (hour_.toLua(stunde))
       --      test()
    end
 
    local function nextMin(tag_neu,stunde_neu,minute_neu)
       if jetzt and jetzt.heute then
          if not stunde then init(tag_neu,stunde_neu) end
-         local tag_alt,stunde_alt,minute_alt=jetzt.heute,jetzt.stunde,jetzt.minute
+         local tag_alt,stunde_alt,minute_alt = jetzt.heute,jetzt.stunde,jetzt.minute
          print(table.concat({'jetzt ist ',tag_neu,"(",stunde_neu,":",minute_neu,')'}))
          if stunde_alt~=stunde_neu then -- stunde speichern
             hour_.append(tag_alt,stunde)
@@ -43,31 +42,18 @@ do
                day_.compile(tag_alt)
             end
             stunde= hour_.get(tag_neu,stunde_neu) -- neue stunde vorbereiten
-         end
-      else
-         print('Minute = ',minute_neu)
-      end
-   end
+         end end end
 
    local function data(datum)
-      --      test()
       if type(datum)=='string' then -- 2025-12-01
          local tmp={}
-         for c in datum:gmatch("[0-9]+") do
-            tmp[#tmp+1]=c
-         end
+         for c in datum:gmatch("[0-9]+") do tmp[#tmp+1]=c end
          if #tmp==3 then
             datum=table.concat(tmp,'-')
             --            print (datum)
-            local tag=day_.get(datum)
-            local lines=day_.toJson(tag)
-            return table.concat(lines,'\n')
-         end
-      end
-      return table.concat({datum,'   ???   '},'\n')
-   end
-
-
+            return table.concat(day_.toJson(day_.get(datum)),'\n')
+         end end
+      return table.concat({datum,'   ???   '},'\n') end
 
    M.data=data
    M.init=init
