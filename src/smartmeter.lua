7f152d1bf127a3162e265a7e2fe3eb8a9628cab9
andreas <andreas@manjaro25>
2025-12-25 10:34:16 +0100

    Umgestellt von info auf data

--- src/smartmeter.lua
+++ src/smartmeter.lua
@@ -10,39 +10,48 @@
 -- SPI UCG ST7735 ??
 do
    local M={}
-   print "lade smartmeter"
-   local data=require 'data'
-   local hour=require 'hour'
+   print "load smartmeter"
+   local hour_=require 'hour'
+   local day_=require 'day'
    local heute
    local gestern
    local stunde={}
-   -- local datum
 
-   -- local jetzt={}
+   local function test()
+      print ''
+      hour_.test()
+      day_.test()
+   end
+
    local function init()
       print ("smartmeter gestartet")
       gestern={}
       heute={}
+--      test()
    end
 
-   local function info(a)
-      hour.test()
-      local antwort={}
-      antwort[#antwort+1]=a
-      antwort[#antwort+1]="Hallo Welt"
-      for _,v in pairs(data.toLines(data.getDay("2025-12-01"))) do -- print (k,v)
-         antwort[#antwort+1]=v
+   local function data(datum)
+      --      test()
+      if type(datum)=='string' then -- 2025-12-01
+         local tmp={}
+         for c in datum:gmatch("[0-9]+") do
+            tmp[#tmp+1]=c
+         end
+         if #tmp==3 then
+            datum=table.concat(tmp,'-')
+            --            print (datum)
+            local tag=day_.get(datum)
+            local lines=day_.toJson(tag)
+            return table.concat(lines,'\n')
+         end
       end
-      antwort[#antwort+1]="-"
-      antwort[#antwort+1]=data.toLine2(data.getHour())
-      print "info"
-      return table.concat(antwort,'\n')
+      return table.concat({datum,'   ???   '},'\n')
    end
 
 
-   M.info=info
+
+   M.data=data
    M.init=init
    print "end smartmeter"
    return M
-
 end
