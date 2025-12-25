0b45ab031ac150cacf3f0d3219c87c4afe90d593
andreas <andreas@manjaro25>
2025-12-25 16:23:36 +0100

    kompaktiert

--- src/util.lua
+++ src/util.lua
@@ -1,24 +1,24 @@
 -- Einige eigene funktionen
 do
    local M={}
-   local function print3d(v0)
-      --   if not v0 then print 'nil' return end -- nullpointer
-      if type(v0) ~= "table" then print(v0) return end
-      if #v0 == 0 then print 'empty{}' return end
-      for k1,v1 in pairs(v0) do
-         if type(v1) == "table" then
-            print(" "..k1.." \t={" )
-            for k2,v2 in pairs(v1) do
-               if type(v2) == "table" then
-                  print("\t "..k2.." \t={" )
-                  for k3,v3 in pairs(v2) do
-                     print("\t\t "..k3.." \t= "..v3 ) end
-                  print("\t ".." \t} " )
-               else
-                  print("\t "..k2.." \t= "..v2 ) end end
-            print("\t ".." \t} " )
-         else
-            print(" "..k1.." \t= "..v1 ) end end end
+   --   local function print3d(v0)
+   --      --   if not v0 then print 'nil' return end -- nullpointer
+   --      if type(v0) ~= "table" then print(v0) return end
+   --      if #v0 == 0 then print 'empty{}' return end
+   --      for k1,v1 in pairs(v0) do
+   --         if type(v1) == "table" then
+   --            print(" "..k1.." \t={" )
+   --            for k2,v2 in pairs(v1) do
+   --               if type(v2) == "table" then
+   --                  print("\t "..k2.." \t={" )
+   --                  for k3,v3 in pairs(v2) do
+   --                     print("\t\t "..k3.." \t= "..v3 ) end
+   --                  print("\t ".." \t} " )
+   --               else
+   --                  print("\t "..k2.." \t= "..v2 ) end end
+   --            print("\t ".." \t} " )
+   --         else
+   --            print(" "..k1.." \t= "..v1 ) end end end
 
    local function fName(name) -- usage: filename=fName(a or b or c)
       local lc=table.concat({name,'.lc'})
@@ -40,6 +40,6 @@ do
    --   printPT()
 
    M.fName=fName
-   M.print3d=print3d
+   --   M.print3d=print3d
    return M
 end
