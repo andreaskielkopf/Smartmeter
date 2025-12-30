014276b4dc2e26ae1ed97f4d1f8d8cc7b5145938
andreas <andreas@manjaro25>
2025-12-30 08:56:19 +0100

    Flanken des IRQ umschaltbar gemacht

--- src/blinker.lua
+++ src/blinker.lua
@@ -1,9 +1,12 @@
 -- Behandle den IRQ und lass zur Kontrolle die LED blinken
 do
    print 'load blinker'
---   local ring=require 'ring'
+   --   local ring=require 'ring'
    local function init()
-      local pinIR, pinNext, pinLED, lastPulse = 7, 6, 4, 0
+      local pinIR,pinNext,pinLED,flanke = 7,6,4,"down"
+      --      local last=0 -- last holds last us
+      if file.exists('flanke_up.flag') then flanke='up' end
+      if file.exists('flanke_both.flag') then flanke='both' end
       -- use pinLED D4 with blue LED to show pulses
       gpio.mode(pinLED, gpio.OUTPUT) gpio.write(pinLED, gpio.LOW)
       gpio.write(pinLED, 0) -- show LED
@@ -12,8 +15,7 @@ do
       --      gpio.mode(pinIR,gpio.INT,gpio.FLOAT) -- extern pullup 20kOhm
       gpio.mode(pinNext,gpio.OPENDRAIN) -- extern geschaltte Masse
       gpio.write(pinNext, 0) -- auf Masse schalten
-      --      local pulsCounter = 0 -- Variable mit der Pulszahl
-      local last=0 -- last holds last us
+
       local function fnIRpuls(level, when, cnt)
          gpio.write(pinLED, 0) -- show LED
          if smart and smart.irPuls then
@@ -29,7 +31,7 @@ do
          --            end end
          gpio.write(pinLED, 1) -- hide LED
       end
-      gpio.trig(pinIR, "both", fnIRpuls) -- starte den IRQ
+      gpio.trig(pinIR, flanke, fnIRpuls) -- starte den IRQ
       gpio.write(pinLED, 1) -- hide LED
       print 'init blinker'
    end
