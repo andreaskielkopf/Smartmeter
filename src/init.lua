b176e09212684cc217c9a7eaddb28d1e3ab26d14
andreas <andreas@manjaro25>
2025-12-30 09:03:42 +0100

    Pin D1 benutzen für Boottest

--- src/init.lua
+++ src/init.lua
@@ -1,10 +1,10 @@
 -- teste beim boot ob D5 auf Masse liegt
 do   
-   local pinTest ,levelBoot = 5, 0  -- D5 testen auf Verbindung zu Masse
+   local pinTest ,levelBoot = 1, 0  -- D5 testen auf Verbindung zu Masse
    gpio.mode(pinTest,gpio.INPUT,gpio.PULLUP) -- Als Input mit internem pullup (20kOhm)
-   print (table.concat({'boot Test D',pinTest,'==',levelBoot}))
+   print (table.concat({'boot Test D',pinTest,'==',levelBoot,' ? '}))
    if gpio.read(pinTest)==levelBoot then
-      print 'boot OK'
+      print 'boot OK'      
       node.flashindex("_init")() -- LFS aktivieren
       require 'start' -- start.lua oder start.lc starten
    else
