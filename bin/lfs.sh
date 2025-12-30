8e5fefa88da5eea80b1d4fc269ff44ab15aa6455
andreas <andreas@manjaro25>
2025-12-30 08:03:02 +0100

    Batch um image zu generieren

--- bin/lfs.sh
+++ bin/lfs.sh
@@ -1,9 +1,10 @@
 
 # Ausführen im ordner bin !
 # das setzt vorraus, dass luac.cross im pfad vorhanden ist
-for F in ../src/*.lua; do luac.cross.int -o "../src/lc/${F%.*}.lc" $F ;done
+cd ../src/
+for F in *.lua; do N="${F%.*}"; ../bin/luac.cross.int -o "./lc/$N.lc" $F;echo -n "$N " ;done
 # for F in ../src/*.lua; do luac.cross.int -s -o "../src/nodebug/${F%.*}.lc" $F ;done
-
-luac.cross.int -o ./smartmeter.img -f ../src/*.lua
+echo
+../bin/luac.cross.int -o ./smartmeter.img -f *.lua
 # luac.cross.int -o ./lc/smartmeter.img -f ./lc/*.lc
 # luac.cross.int -s -o ./nodebug/smartmeter.img -f ./nodebug/*.lc
