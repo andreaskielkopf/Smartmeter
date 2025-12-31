9f38ab983b9cbae320dbbc925f770142d99a76e9
andreas <andreas@manjaro25>
2025-12-31 15:11:01 +0100

    leerzeile

--- bin/lfs.sh
+++ bin/lfs.sh
@@ -4,7 +4,7 @@
 cd ../src/
 for F in *.lua; do N="${F%.*}"; ../bin/luac.cross.int -o "./lc/$N.lc" $F;echo -n "$N " ;done
 # for F in ../src/*.lua; do luac.cross.int -s -o "../src/nodebug/${F%.*}.lc" $F ;done
-echo
+echo ' '
 ../bin/luac.cross.int -o ./smartmeter.img -f *.lua
 # luac.cross.int -o ./lc/smartmeter.img -f ./lc/*.lc
 # luac.cross.int -s -o ./nodebug/smartmeter.img -f ./nodebug/*.lc
