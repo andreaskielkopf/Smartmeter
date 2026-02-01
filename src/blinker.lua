23712bf9f2fa6deb722c99a70685704be8772828
andreas <andreas.kielkopf@gmail.com>
2026-02-01 18:48:12 +0100

    update verbessern

--- src/blinker.lua
+++ src/blinker.lua
@@ -1,12 +1,13 @@
 -- Behandle den IRQ und lass zur Kontrolle die LED blinken
 do
-   print 'blinker b06'
-   ver[#ver+1]="b06"
+   print 'blinker b0f'
+   ver[#ver+1]="b0f"
    local ring=require 'ring'
    local function init()
       local pinGND,pinIR,pinLED,flanke= 2,3,4,"down" -- pinIR:1,2,3,5,6,7 pinGND:0,1,2,3,5,6,7 pinLED:4
-      local last -- last holds last us for ring
-      -- local min= 5000 -- 5 ms
+      local last,c,dif -- holds last us for ring
+      local vier=0x40000000
+      local min= 3000 -- 3 ms entspricht 120kW als obere Messgrenze
       -- use pinLED with blue LED to show pulses
       gpio.mode(pinLED, gpio.OUTPUT)
       gpio.write(pinLED, gpio.LOW) -- show LED
@@ -22,16 +23,24 @@ do
 
       local function fnIRpuls(level, when, cnt)
          gpio.write(pinLED, gpio.LOW) -- show LED
+         if last then dif= when-last while dif<0 do dif= dif+vier end end -- us>0
          if smart and smart.irPuls then
             --         print( tmr.time(),pulsCounter, level, cnt, pulse - lastPulse  )
-            smart.irPuls(cnt)
+            if last and dif>=min then smart.irPuls(6,dif/1000) end -- WattMinuten,ms
          end
-         if ring and ring.push and last then do            
-               if cnt~=1 then ring.push(':') ring.push(cnt) end
-               local zeit= when-last
---               ring.push(level==0 and zeit or -zeit)
-               ring.push(string.format("%x", when/0x10000))
-            end end 
+         if ring and ring.push and last then do
+            --            if cnt~=1 then ring.push(':') ring.push(cnt) end
+            --               ring.push(level==0 and zeit or -zeit)
+            --               ring.push(string.format("%x", when/0x10000))
+            if when<last then
+               ring.push("---------------------->")
+               ring.push(dif/1000)
+            end
+--            if dif>=min then
+--               ring.push(dif/1000) --ms
+--            else ring.push(' ') -- 0 ms
+--            end
+         end end
          last= when
          gpio.write(pinLED, gpio.HIGH) -- hide LED
       end
--- src/updateLFS.lua
+++ src/updateLFS.lua
@@ -1,27 +1,28 @@
 do
-   print 'load updateLFS'
+   print 'updateLFS f05'
+   ver[#ver+1]="f05"
    -- Teste ob ein Update des LFS notwendig ist, und führe ihn aus
    local function update()
-      print 'auf update testen'
-      local update_flag, remove_flag, lfs_img= 'update.flag', 'remove.flag', 'smartmeter.img'
-      if file.exists(lfs_img) then -- wenn ein neues smartmeter.img da ist
-         print(table.concat({lfs_img, ' exists'}))
-         if file.exists(update_flag) then -- und es ist auch ein update.flag da
-            print (table.concat({update_flag,' exists'}))
-            if file.exists(remove_flag) then -- im 2.Durchlauf Aufräumen
-               print (table.concat({remove_flag,' exists'}))
-               file.remove(lfs_img) -- nach dem Update das image entfernen
-               file.remove(update_flag) -- nach dem update das update.flag entfernen
-               file.remove(remove_flag) -- und zuletzt das remove.flag auch entfernen
-               print (table.concat({'removed',lfs_img,update_flag,remove_flag},', '))
-            else -- Im 1.Durchlauf den Update durchführen
-               print (table.concat({'create ',remove_flag}))
-               -- remove.flag setzen damit der 2.Durchlauf anders ablaufen kann
-               rm= file.open(remove_flag,'w') rm:writeline(remove_flag) rm:close() rm= nil
-               print (table.concat({'reload ', lfs_img})) -- letzte Meldung vor dem Update
-               node.LFS.reload(lfs_img) -- immage laden und programm neu starten
-               --               print 'hier sollten wir eigentlich nicht mehr vorbeikommen'
-            end end end end
+      --      print 'auf update testen'
+      local update_flag, remove_flag, lfs_img, rm= 'update.flag', 'remove.flag', 'smartmeter.img'
+      if file.exists(lfs_img) -- wenn ein neues smartmeter.img da ist
+         --         print(table.concat({lfs_img, ' exists'}))
+         and file.exists(update_flag) then -- und es ist auch ein update.flag da
+         --            print (table.concat({update_flag,' exists'}))
+         if not file.exists(remove_flag) then -- Im 1.Durchlauf den Update durchführen
+            --               print (table.concat({'create ',remove_flag}))
+            -- remove.flag setzen damit der 2.Durchlauf anders ablaufen kann
+            rm= file.open(remove_flag,'a') rm:writeline(remove_flag) rm:close() rm= nil
+            --               print (table.concat({'reload ', lfs_img})) -- letzte Meldung vor dem Update
+            node.LFS.reload(lfs_img) -- immage laden und programm neu starten
+            --               print 'hier sollten wir eigentlich nicht mehr vorbeikommen'
+         else -- im 2.Durchlauf Aufräumen
+            --               print (table.concat({remove_flag,' exists'}))
+            file.remove(lfs_img) -- nach dem Update das image entfernen
+            file.remove(update_flag) -- nach dem update das update.flag entfernen
+            file.remove(remove_flag) -- und zuletzt das remove.flag auch entfernen
+            --               print (table.concat({'removed',lfs_img,update_flag,remove_flag},', '))
+         end end end
 
    update()
    print 'end updateLFS'
